894b114cd9b89b1312ee541465fcee07a3df9452
Alexandra <alya.romanovna@mail.ru>
2021-04-08 20:43:06 +0300

    fixed 1

--- work_file.py
+++ work_file.py
@@ -3,11 +3,14 @@
 import json
 
 di = {}
-with open('purchase_log.txt', encoding='utf-8') as file:
+with open('purchase_log.txt', 'r', encoding='utf-8') as file:
     for i, line in enumerate(file):
-        line = line.strip()
-        di = json.loads(line)
-
+        current = json.loads(line)
+        for k, v in current.items():
+            key = current['user_id']
+            value = current['category']
+            if key != 'user_id':
+                di[key] = value
 
 # Для каждого user_id в файле visit_log.csv определите третий столбец с категорией покупки
 # (если покупка была, сам файл visit_log.csv изменять не надо).
@@ -15,6 +18,10 @@ with open('purchase_log.txt', encoding='utf-8') as file:
 
 with open('visit_log.csv', 'r', encoding='utf-8') as f1:
     with open('funnel.csv', 'w') as f2:
-        for line in f1:
-            if 'context' in line:
-                f2.write(line)
+        for i, line in enumerate(f1):
+            for k, v in di.items():
+                print(line)
+            if i > 10:
+                break
+
+                
